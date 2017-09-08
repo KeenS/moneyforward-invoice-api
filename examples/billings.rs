090396e5c1516d801433d1e2bb2cd1d6d1888556
Sunrin SHIMURA (keen) <3han5chou7@gmail.com>
2017-09-08 17:25:19 +0900

    add an example of billing CRUD

---
+++ examples/billings.rs
@@ -0,0 +1,72 @@
+//! Envs:
+//! "MF_INVOICE_ACCESS_TOKEN" -- access token
+extern crate moneyforward_invoice_api as mf;
+extern crate env_logger;
+extern crate native_tls;
+
+use mf::{Client, NewBilling, NewBillingItem, NewPartner, UpdateBilling, UpdateBillingItem};
+use std::env;
+
+fn main() {
+    env_logger::init().unwrap();
+
+    let token = env::var("MF_INVOICE_ACCESS_TOKEN").unwrap();
+    let mut client = Client::new(token).unwrap();
+
+    let partner = client
+        .create_partner(NewPartner {
+            name: "サンプル取引先".into(),
+            ..Default::default()
+        })
+        .unwrap()
+        .unwrap();
+
+    let billing = client
+        .create_billing(NewBilling {
+            department_id: partner.departments[0].clone().id,
+            items: vec![NewBillingItem { ..Default::default() }],
+            ..Default::default()
+        })
+        .unwrap()
+        .unwrap();
+
+    println!("created billing: {:#?}", billing);
+
+    let billing = client
+        .update_billing(
+            &billing.id,
+            UpdateBilling {
+                memo: Some("更新しました".into()),
+                items: vec![
+                    UpdateBillingItem {
+                        id: Some(billing.items[0].clone().id),
+                        _destroy: true,
+                        ..Default::default()
+                    },
+                    UpdateBillingItem {
+                        name: Some("新しい品目".into()),
+                        ..Default::default()
+                    },
+                ],
+                ..Default::default()
+            },
+        )
+        .unwrap()
+        .unwrap();
+
+    println!("updated billing: {:#?}", billing);
+
+    let billings = client.list_billings(1, 100).unwrap().unwrap();
+    println!("list metadata: {:#?}", billings.meta);
+    println!("list :{:#?}", billings.billings);
+
+    let billings = client
+        .search_billings(1, 100, "サンプル", None, None, None)
+        .unwrap()
+        .unwrap();
+    println!("search metadata: {:#?}", billings.meta);
+    println!("search result :{:#?}", billings.billings);
+
+    client.delete_billing(&billing.id).unwrap().unwrap();
+    println!("deleted the billing");
+}
